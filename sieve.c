c4a095d9db2131c8539cacbf5cadeb5e6e4d1337
Urs Thuermann <urs@isnogud.escape.de>
1992-03-19 16:39:54 +0100

    White-space changes
    * Change indentation width from 3 to 8.
    * Add line-breaks after opening braces.

--- sieve.c
+++ sieve.c
@@ -6,22 +6,24 @@
 int flags[MAX];
 
 int main(void)
-{  register int i, k, prime, count;
+{
+	register int i, k, prime, count;
 
-   for(i = 0; i < MAX; i++)
-      flags[i] = TRUE;
-   /* the first prime number is even. it is 2 */
-   count = 1;
-   /* find all odd prime numbers by sieving */
-   for(i = 0; i < MAX; i++)
-      if(flags[i])
-      {  prime = i + i + 3;
-         for(k = i + prime; k < MAX; k += prime)
-            flags[k] = FALSE;
-         count++;
-      }
+	for(i = 0; i < MAX; i++)
+		flags[i] = TRUE;
+	/* the first prime number is even. it is 2 */
+	count = 1;
+	/* find all odd prime numbers by sieving */
+	for(i = 0; i < MAX; i++)
+		if(flags[i])
+		{
+			prime = i + i + 3;
+			for(k = i + prime; k < MAX; k += prime)
+				flags[k] = FALSE;
+			count++;
+		}
 
-   printf("n=%d\n", count);
-   getchar();
-   return(0);
+	printf("n=%d\n", count);
+	getchar();
+	return(0);
 }
