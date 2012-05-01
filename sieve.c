ec466292eba9f7ba52f9814e7f5819c1703ad147
Urs Thuermann <urs@isnogud.escape.de>
2012-05-01 07:59:34 +0200

    Add comments

--- sieve.c
+++ sieve.c
@@ -1,9 +1,13 @@
 /*
- * $Id: sieve.c,v 1.3 2012/05/01 05:58:11 urs Exp $
+ * $Id: sieve.c,v 1.4 2012/05/01 05:59:34 urs Exp $
  */
 
 #include <stdio.h>
 
+/* The array to hold the sieve.  There's a flag for each odd number
+ * above 2, i.e. flag[i] is for 2 * i + 3.  We provide space for 5000
+ * flags, i.e. for prime numbers up to 4999 * 2 + 3 = 10001.
+ */
 #define MAX 5000
 
 static int flags[MAX];
@@ -22,6 +26,7 @@ int main(void)
 	for (i = 0; i < MAX; i++)
 		if (flags[i]) {
 			prime = 2 * i + 3;
+			/* Discard all odd multiples of prime. */
 			for (k = i + prime; k < MAX; k += prime)
 				flags[k] = 0;
 			count++;
