79fd4a4e4cb5da146ab495831db62ca414131ca9
Urs Thuermann <urs@isnogud.escape.de>
2012-05-01 08:00:29 +0200

    Dynamically allocate the sieve array
    Use only one bit per number.
    Use functions set() and test() to access sieve flags.
    Move sieve code into a separate function sieve().

--- sieve.c
+++ sieve.c
@@ -1,38 +1,75 @@
 /*
- * $Id: sieve.c,v 1.4 2012/05/01 05:59:34 urs Exp $
+ * $Id: sieve.c,v 1.5 2012/05/01 06:00:29 urs Exp $
  */
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 
-/* The array to hold the sieve.  There's a flag for each odd number
- * above 2, i.e. flag[i] is for 2 * i + 3.  We provide space for 5000
- * flags, i.e. for prime numbers up to 4999 * 2 + 3 = 10001.
+/* The sieve is a bit array with one bit for each odd number above 2,
+ * i.e. bit i is for 2 * i + 3.  We provide space for 5000 flags,
+ * i.e. for prime numbers up to 4999 * 2 + 3 = 10001.
  */
 #define MAX 5000
 
-static int flags[MAX];
+static char *alloc(unsigned int n);
+static void set(char *s, unsigned int n);
+static int test(const char *s, unsigned int n);
+static int sieve(char *s, unsigned int n);
 
 int main(void)
 {
-	int i, k, prime, count;
+	unsigned int n = MAX;
+	int count;
+	char *s;
 
-	for (i = 0; i < MAX; i++)
-		flags[i] = 1;
+	if (!(s = alloc(n))) {
+		perror("malloc");
+		exit(1);
+	}
+	count = sieve(s, n);
+	printf("n = %d\n", count);
+	free(s);
+
+	return 0;
+}
+
+static int sieve(char *s, unsigned int n)
+{
+	unsigned int i, k, prime;
+	int count;
 
 	/* The first prime number is even. It is 2. */
 	count = 1;
 
 	/* Find all odd prime numbers by sieving. */
-	for (i = 0; i < MAX; i++)
-		if (flags[i]) {
+	for (i = 0; i < n; i++)
+		if (!test(s, i)) {
 			prime = 2 * i + 3;
 			/* Discard all odd multiples of prime. */
-			for (k = i + prime; k < MAX; k += prime)
-				flags[k] = 0;
+			for (k = i + prime; k < n; k += prime)
+				set(s, k);
 			count++;
 		}
 
-	printf("n=%d\n", count);
+	return count;
+}
 
-	return 0;
+static char *alloc(unsigned int n)
+{
+	return calloc((n + CHAR_BIT - 1) / CHAR_BIT, 1);
+}
+
+static void set(char *s, unsigned int n)
+{
+	int chr = n / CHAR_BIT;
+	int bit = n % CHAR_BIT;
+	s[chr] |= 1 << bit;
+}
+
+static int test(const char *s, unsigned int n)
+{
+	int chr = n / CHAR_BIT;
+	int bit = n % CHAR_BIT;
+	return (s[chr] >> bit) & 1;
 }
