07e924e6378406b31553146bc84bcd3f03d68d1a
Urs Thuermann <urs@isnogud.escape.de>
2012-05-01 07:58:11 +0200

    Code cleanup
    * Remove getchar() before program exit.
    * Remove register in variable declaration.
    * Remove inclusion of stddef.h.
    * Use 0 and 1 instead of FALSE and TRUE.
    * Make flags[] static.
    * White-space and brace placement changes.
    * Minor edits in comments.
    * Add RCS Id.

--- sieve.c
+++ sieve.c
@@ -1,29 +1,33 @@
+/*
+ * $Id: sieve.c,v 1.3 2012/05/01 05:58:11 urs Exp $
+ */
+
 #include <stdio.h>
-#include <stddef.h>
 
 #define MAX 5000
 
-int flags[MAX];
+static int flags[MAX];
 
 int main(void)
 {
-	register int i, k, prime, count;
+	int i, k, prime, count;
 
-	for(i = 0; i < MAX; i++)
-		flags[i] = TRUE;
-	/* the first prime number is even. it is 2 */
+	for (i = 0; i < MAX; i++)
+		flags[i] = 1;
+
+	/* The first prime number is even. It is 2. */
 	count = 1;
-	/* find all odd prime numbers by sieving */
-	for(i = 0; i < MAX; i++)
-		if(flags[i])
-		{
-			prime = i + i + 3;
-			for(k = i + prime; k < MAX; k += prime)
-				flags[k] = FALSE;
+
+	/* Find all odd prime numbers by sieving. */
+	for (i = 0; i < MAX; i++)
+		if (flags[i]) {
+			prime = 2 * i + 3;
+			for (k = i + prime; k < MAX; k += prime)
+				flags[k] = 0;
 			count++;
 		}
 
 	printf("n=%d\n", count);
-	getchar();
-	return(0);
+
+	return 0;
 }
