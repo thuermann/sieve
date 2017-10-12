12d05692ab0411ba1e849af69ba56b833df16709
Urs Thuermann <urs@isnogud.escape.de>
2017-10-12 10:51:31 +0200

    Use uintmax_t and a typedef for prime numbers and counter

--- sieve.c
+++ sieve.c
@@ -1,16 +1,20 @@
 /*
- * $Id: sieve.c,v 1.6 2012/05/01 06:01:37 urs Exp $
+ * $Id: sieve.c,v 1.7 2017/10/12 08:51:31 urs Exp $
  */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <inttypes.h>
 #include <limits.h>
 
-static char *alloc(unsigned int n);
-static void set(char *s, unsigned int n);
-static int test(const char *s, unsigned int n);
-static int sieve(char *s, unsigned int n);
+typedef uintmax_t int_t;
+#define PRI PRIuMAX
+
+static char *alloc(int_t n);
+static void set(char *s, int_t n);
+static int test(const char *s, int_t n);
+static int_t sieve(char *s, int_t n);
 
 static void usage(const char *name)
 {
@@ -21,8 +25,8 @@ int main(int argc, char **argv)
 {
 	char *name = argv[0];
 	int verbose = 0;
-	int count;
-	unsigned int n, i;
+	int_t n, i;
+	int_t count;
 	char *s;
 
 	if (argc > 1 && strcmp(argv[1], "-v") == 0) {
@@ -38,7 +42,7 @@ int main(int argc, char **argv)
 	 * above 2, i.e. bit i is for n = 2 * i + 3.  Thus, we need
 	 * (n-3)/2 + 1 bits to find primes up to and including n.
 	 */
-	n = atoi(argv[1]);
+	n = strtoull(argv[1], NULL, 0);
 	n = (n - 3) / 2 + 1;
 
 	if (!(s = alloc(n))) {
@@ -46,22 +50,22 @@ int main(int argc, char **argv)
 		exit(1);
 	}
 	count = sieve(s, n);
-	printf("n = %d\n", count);
+	printf("n = %"PRI"\n", count);
 	if (verbose) {
 		puts("2");
 		for (i = 0; i < n; i++)
 			if (!test(s, i))
-				printf("%d\n", 2 * i + 3);
+				printf("%"PRI"\n", 2 * i + 3);
 	}
 	free(s);
 
 	return 0;
 }
 
-static int sieve(char *s, unsigned int n)
+static int_t sieve(char *s, int_t n)
 {
-	unsigned int i, k, prime;
-	int count;
+	int_t i, k, prime;
+	int_t count;
 
 	/* The first prime number is even. It is 2. */
 	count = 1;
@@ -79,21 +83,21 @@ static int sieve(char *s, unsigned int n)
 	return count;
 }
 
-static char *alloc(unsigned int n)
+static char *alloc(int_t n)
 {
 	return calloc((n + CHAR_BIT - 1) / CHAR_BIT, 1);
 }
 
-static void set(char *s, unsigned int n)
+static void set(char *s, int_t n)
 {
-	int chr = n / CHAR_BIT;
-	int bit = n % CHAR_BIT;
+	int_t chr = n / CHAR_BIT;
+	int   bit = n % CHAR_BIT;
 	s[chr] |= 1 << bit;
 }
 
-static int test(const char *s, unsigned int n)
+static int test(const char *s, int_t n)
 {
-	int chr = n / CHAR_BIT;
-	int bit = n % CHAR_BIT;
+	int_t chr = n / CHAR_BIT;
+	int   bit = n % CHAR_BIT;
 	return (s[chr] >> bit) & 1;
 }
